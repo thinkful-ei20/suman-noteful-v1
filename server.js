1e204d929dbbfc3d916bc146be488e3a1ad0bd57
Suman Bandita <sumanbandita@gmail.com>
2018-04-17 17:41:39 -0400

    Done but Bonus

--- server.js
+++ server.js
@@ -7,39 +7,35 @@ const {requestLogger} = require('./middleware/logger');
 
 // Simple In-Memory Database
 const data = require('./db/notes');
+const simDB = require('./db/simDB');  // <<== add this
+const notes = simDB.initialize(data); // <<== and this
 
 // Create an Express application
 const app = express();
 
 // Create a static webserver
 app.use(express.static('public'));
+app.use(express.json());
 app.use(requestLogger);
 // Get All (and search by query)
-app.get('/api/notes', (req, res) => {
+app.get('/api/notes', (req, res, next) => {
+  const {searchTerm} = req.query;
 
-  // Send JSON response (data is an array of objects)
-  // res.json(data);
-
-  /**
-   * Implement Search
-   * Below are 2 solutions: verbose and terse. They are functionally identical but use different syntax
-   *
-   * Destructure the query string property in to `searchTerm` constant
-   * If searchTerm exists...
-   * then `filter` the data array where title `includes` the searchTerm value
-   * otherwise return `data` unfiltered
-   */
-
-  // Verbose solution
-  const searchTerm = req.query.searchTerm;
-  if (searchTerm) {
-    let filteredList = data.filter(function(item) {
-      return item.title.includes(searchTerm);
-    });
-    res.json(filteredList);
-  } else {
-    res.json(data);
-  }
+  notes.filter(searchTerm,(err,list) => {
+    if(err){
+      return next(err);
+    }
+    res.json(list);
+  });
+  // const searchTerm = req.query.searchTerm;
+  // if (searchTerm) {
+  //   let filteredList = data.filter(function(item) {
+  //     return item.title.includes(searchTerm);
+  //   });
+  //   res.json(filteredList);
+  // } else {
+  //   res.json(data);
+  // }
 
   // Terse solution
   // const { searchTerm } = req.query;
@@ -54,19 +50,48 @@ app.get('/api/notes', (req, res) => {
 // });
 
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res,next) => {
   const id = req.params.id;
-
-  // Verbose solution
-  let note = data.find(function(item) {
-    return item.id === Number(id);
+  notes.find(id,(err,item)=>{
+    if(err){
+      return next(err);
+    }
+    res.json(item);
   });
-  res.json(note);
+  // // Verbose solution
+  // let note = data.find(function(item) {
+  //   return item.id === Number(id);
+  // });
+  // res.json(note);
 
   // Terse solution
   // res.json(data.find(item => item.id === Number(id)));
 
 });
+
+app.put('/api/notes/:id',(req,res,next)=>{  
+  const id = req.params.id;  
+  const updateObj ={};
+  const updateFields = ['title','content'];
+
+  updateFields.forEach(field => {
+    if(field in req.body){
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  notes.update(id,updateObj,(err,item)=>{  
+    if(err){
+      return next(err);
+    }
+    if(item){
+      res.json(item);
+    }else{
+      next();
+    }
+  });
+});
+
 
 //ERROR HANDLING 
 app.use(function (req, res, next) {
@@ -83,12 +108,17 @@ app.use(function (err, req, res, next) {
   });
 });
 
+
+
 // Listen for incoming connections
 app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
 });
+
+
+
 // 'use strict';
 
 // const data = require('./db/notes');
